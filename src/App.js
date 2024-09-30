2290e5f35aa5c71e971339be85bc470594ea3092
kjohh <kjohh-tool@yahoo.com>
2024-09-30 16:04:57 +0800

    Add GTM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import WowMomentsApp from './WowMomentsApp';
+import GTMNoScript from './GTMNoScript';
 
 function App() {
   return (
     <div className="App">
+      <GTMNoScript />
       <WowMomentsApp />
     </div>
   );
---
+++ src/GTMNoScript.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const GTMNoScript = () => (
+  <noscript>
+    <iframe 
+      src="https://www.googletagmanager.com/ns.html?id=GTM-MQFB69K3"
+      height="0" 
+      width="0" 
+      style={{display: 'none', visibility: 'hidden'}}
+    ></iframe>
+  </noscript>
+);
+
+export default GTMNoScript;
